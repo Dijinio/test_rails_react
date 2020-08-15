aa985d5525052bd80cae1d736711fe69493f9fc0
dijinio <dijinio@gmail.com>
2020-08-15 17:17:07 +0200

    fix2

---
+++ public/precache-manifest.87a601290aa9e4e9bf71e6cf64705f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2003cde3120f9893a8e4e7758e6c89bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c374eb137fd478b911ff",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "f51104b585edc48909fc",
+    "url": "/static/js/2.80f619ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80f619ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c374eb137fd478b911ff",
+    "url": "/static/js/main.7f5a9091.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
